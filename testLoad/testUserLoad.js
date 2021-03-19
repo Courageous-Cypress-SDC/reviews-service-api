0c83ff230eef4f62aa6ac9e7fe8e3dfaecd3a02b
rfduong <ronnyduong@gmail.com>
2021-03-18 23:53:15 -0700

    Added validators to user load, can reuse for loading reviews

--- testLoad/testUserLoad.js
+++ testLoad/testUserLoad.js
@@ -16,58 +16,137 @@ let weirdRows = [];
 //   database: 'testCSVLoadOne'
 // });
 
-stream
-  .pipe(es.split())
-  .pipe(
-    es.mapSync(function(line) {
-        let data = line.split(',');
-        if (data.length !== 12) {
-          //id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness
-          weirdRows.push(totalLines);
-        }
-        let id = data[0];
-        let productId = data[1];
-        let rating = data[2];
-        let date = data[3];
-        let summary = data[4];
-        let body = data[5];
-        let recommend = data[6];
-        let reported = data[7];
-        let reviewer_name = data[8];
-        let reviewer_email = data[9];
-        let response = data[10];
-        let helpfulness = data[11];
-        if (totalLines != 0) {
-          rows.push([id, productId, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness]);
-        }
-        totalLines++;
-      })
-      .on('error', (error) => {
-        console.error(error);
-      })
-      .on('end', () => {
-        console.log('read entire file');
-        console.log(totalLines);
-        console.log(rows.length);
-        console.log('weird rows: ', JSON.stringify(weirdRows));
-        // const connection = mysql.createConnection({
-        //   host: 'localhost',
-        //   user: 'root',
-        //   password: '123456',
-        //   database: 'testCSVLoadOne'
-        // });
+let productIds = new Set();
+// connection.connect((error) => {
+//   if (error) {
+//     console.error(error);
+//   } else {
+//     let query = 'select id from products';
+//     connection.query(query, (err, response) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         for (let i = 0; i < response.length; i++) {
+//           productIds.add(response[i].id);
+//         }
+//         loadUsers();
+//       }
+//     });
+//   }
+// });
+loadUsers();
 
-        // // open the connection
-        // connection.connect(error => {
-        //   if (error) {
-        //     console.error(error);
-        //   } else {
-        //     let query =
-        //       'INSERT INTO reviewers (id, name) VALUES ?';
-        //     connection.query(query, [rows], (error, response) => {
-        //       console.log(error || response);
-        //     });
-        //   }
-        // });
-      })
-  );
+function loadUsers() {
+  stream
+    .pipe(es.split())
+    .pipe(
+      es.mapSync(function(line) {
+          let data = line.split(',');
+          if (data.length !== 12) {
+            //id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness
+            weirdRows.push(totalLines);
+          }
+          let id = data[0];
+          if (parseInt(id) === 1) {
+            console.log('hi');
+            totalLines++;
+            return;
+          }
+          let productId = data[1];
+          // if (!productIds.has(productId)) {
+          //   console.log('no product id found');
+          //   totalLines++;
+          //   return;
+          // }
+          let rating = data[2];
+          let parsedRating = parseInt(rating);
+          if (isNaN(parsedRating) || parsedRating < 0 || parsedRating > 5) {
+            console.log('invalid rating');
+            totalLines++;
+            return;
+          }
+          let date = data[3];
+          let summary = data[4].split('"')[1];
+          if (summary.length > 60) {
+            // console.log('summary too long');
+            // console.log(summary, summary.length);
+            summary = summary.slice(0, 60);
+            // totalLines++;
+            // return;
+          }
+          let body = data[5].split('"')[1];
+          if (body.length < 50) {
+            console.log('body too short');
+            totalLines++;
+            return;
+          } else if (body.length > 1000) {
+            console.log('body too long');
+            body = body.slice(0, 1000);
+          }
+          let recommend = data[6];
+          let reported = data[7];
+          let reviewer_name = data[8];
+          if (reviewer_name.length > 60) {
+            console.log('reviewer name too long');
+            totalLines++;
+            return;
+          }
+          let reviewer_email = data[9].split('"')[1];
+          const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          if (reviewer_email.length > 60) {
+            console.log('reviewer email too long');
+            totalLines++;
+            return;
+          } else if (!emailRegex.test(reviewer_email)) {
+            console.log('invalid email format');
+            totalLines++;
+            return;
+          }
+          let response = data[10];
+          if (response && response.length > 1000) {
+            console.log('response is too long');
+            totalLines++;
+            return;
+          }
+          let helpfulness = data[11];
+          let parsedHelpful = parseInt(helpfulness);
+          if (isNaN(parsedHelpful)) {
+            console.log('invalid helpfulness');
+            totalLines++;
+            return;
+          }
+          if (totalLines != 0) {// && productIds.has(productId)) {
+            rows.push([id, productId, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness]);
+          }
+          totalLines++;
+        })
+        .on('error', (error) => {
+          console.error(error);
+        })
+        .on('end', () => {
+          console.log('read entire file');
+          console.log(totalLines);
+          console.log(rows.length);
+          console.log('weird rows: ', JSON.stringify(weirdRows));
+          // const connection = mysql.createConnection({
+          //   host: 'localhost',
+          //   user: 'root',
+          //   password: '123456',
+          //   database: 'testCSVLoadOne'
+          // });
+
+          // // open the connection
+          // connection.connect(error => {
+          //   if (error) {
+          //     console.error(error);
+          //   } else {
+          //     let query =
+          //       'INSERT INTO reviewers (id, name) VALUES ?';
+          //     connection.query(query, [rows], (error, response) => {
+          //       console.log(error || response);
+          //     });
+          //   }
+          // });
+        })
+    );
+}
